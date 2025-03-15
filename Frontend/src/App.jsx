56e405cdaf9092318e12258af546c018c76237b6
vivek <vivekbilla822@gmail.com>
2025-03-15 18:14:56 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-black font-bold text-center p-4 text-2xl">  
+        <h1 className="">code collaborator</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
